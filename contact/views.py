c30e5001fe5aaf386ddc8e14c3b02e5e8f15fd0c
briceedimo <edimobrice237@gmail.com>
2026-01-19 20:20:08 +0100

    recuperation

--- contact/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import get_object_or_404, redirect, render
-from django.contrib import messages
-from django.template.context_processors import request
-from django.http import Http404
-from contact.models import Contact
-# Create your views here.
