adec9814588c9a3ac016c55d9f386c5cb0082c99
Romeo <bonworomeo@gmail.com>
2026-01-19 20:26:27 +0100

    recupearation du code sur develop

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import index , store , create
+from .views import index , store , delete, show, edit #, create
 urlpatterns = [
     path('', index, name="contact"),
     # path('create/', create, name="create_contact"),
-    path('store/', store, name="store_contact")
+    path('store/', store, name="store_contact"),
+    path('delete-contact/<int:id>/', delete, name="delete_contact"),
+    path('show-contact/<int:id>/', show, name="show_contact"),
+    path('edit-contact/<int:id>/', edit, name="edit_contact")
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib import messages
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.template.context_processors import request
 from contact.models import Contact
 # Create your views here.
 def index(request):
-    Contacts = Contact.objects.all()
-    return render(request, "contact/contacts.html", {"contacts": Contacts})
+    Contacts = Contact.objects.all().order_by('-id')
+    
+    
+    # ---- Pagination -----
+    #items = Contact.objects.all().order_by('id') # Get all items
+    paginator = Paginator(Contacts, 2) # 10 items per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number) # Get page object for current page
+    
+    
+    return render(request, "contact/contacts.html", {"contacts": Contacts,  'page_obj': page_obj })
 
 
 # def create(request) : 
@@ -41,8 +51,39 @@ def store(request) :
     return render(request=request, template_name='contact/create_contact.html')
 
 
-def edit(request) : 
-    pass
+def edit(request, id:int) : 
+    contact = get_object_or_404(Contact, id=id)
+    if request.method == 'POST':
+        nom = request.POST['nom']
+        prenom = request.POST['prenom']
+        email = request.POST['email']
+        pays = request.POST['pays']
+        phone = request.POST['phone']
+        ville = request.POST['ville']
+        rue = request.POST['rue']
+        quartier = request.POST['quartier']
+
+
+        contact.nom = nom
+        contact.prenom = prenom
+        contact.email = email
+        contact.pays = pays
+        contact.phone = phone
+        contact.ville = ville
+        contact.rue = rue
+        contact.quartier = quartier
+
+        contact.save()
+        
+        return redirect('contact')
+    return render(request, 'contact/edit.html', {'contact':contact})
+        
+
+def show(request, id:int) : 
+    contact = get_object_or_404(Contact, id=id)
+    
+    return render (request=request, template_name='contact/show.html', context={'contact':contact})
+
 
 def delete(request, id:int) : 
     try:
@@ -55,6 +96,8 @@ def delete(request, id:int) :
         print(f"{ve}")
     except Exception as e :
         print(f"{e}")
+
+
 
 # def add(a,b):
 #     return a + b
