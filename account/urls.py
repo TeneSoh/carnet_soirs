facba1e4a0e184fd95bcd87bbfe85d8d9b6e7b69
= <tenedouglas237@gmail.com>
2026-02-04 18:49:47 +0100

    recuperation du code du formulaire

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
+    # path('', include('django.contrib.auth.urls')),
+    path('', auth_view.LoginView.as_view(), name='login'),
+    
 ]
