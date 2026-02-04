c4f923be7124c186bdfc4bcb66aeff31b1076cc3
= <tenedouglas237@gmail.com>
2026-02-04 19:02:16 +0100

    recuperation du code refister

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,4 @@ from django.contrib.auth import views as auth_view
 urlpatterns = [
     # path('', include('django.contrib.auth.urls')),
     path('', auth_view.LoginView.as_view(), name='login'),
-    
 ]
