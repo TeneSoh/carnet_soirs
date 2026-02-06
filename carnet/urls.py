83de9eff817a82dbf32aef011596827b0694afed
= <tenedouglas237@gmail.com>
2026-02-06 20:05:02 +0100

    recuperation

--- carnet/urls.py
+++ carnet/urls.py
@@ -18,9 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', include('account.urls')),
-    # path('contacts/', include('contact.urls')),
-    path('', include('contact.urls')),
+    # path('admin/', admin.site.urls),
+    path('', include('account.urls')),
+    path('contacts/', include('contact.urls')),
 ]
 
