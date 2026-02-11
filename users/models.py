48342d6a46dce5a2fed3a180a21951f3bfea678f
= <tenedouglas237@gmail.com>
2026-02-11 18:49:27 +0100

    start personnal authentication

---
+++ users/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class UserManager(BaseUserManager):
+    user_in_migration = True
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("the email field is required")
+        email = self.normalize_email(email=email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_admin', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=30, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()  # type: ignore
+
+    def __str__(self) -> str:
+        return self.email
---
+++ users/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import redirect, render
+# from django.contrib.auth import login, logout, authenticate
+# from users.forms import LoginForm, RegisterForm
+
+# Create your views here.
+
